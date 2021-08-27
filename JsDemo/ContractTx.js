607247d6dfd8e9a0346c1cf5d9e37b7b315110cb
Owen Zhang <38493437+superboyiii@users.noreply.github.com>
2021-08-27 10:22:07 +0800

    fix remark14

--- JsDemo/ContractTx.js
+++ JsDemo/ContractTx.js
@@ -26,7 +26,7 @@ let inputObj2 = {
 
 raw.addOutput(new Neon.tx.TransactionOutput(outPutObj1));
 raw.inputs[0] = new Neon.tx.TransactionInput(inputObj);
-raw.addAttribute(Neon.tx.TxAttrUsage.Remark14,"Your N3 address");
+raw.addAttribute(Neon.tx.TxAttrUsage.Remark14, u.str2hexstring("Your N3 address"));
 
 raw.addOutput(new Neon.tx.TransactionOutput(outPutObj2));
 raw.inputs[1] = new Neon.tx.TransactionInput(inputObj2);
